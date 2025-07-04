f14c4e04148c6163e3d9ac2458d031da5a5fdeab
ASTR <theassettrack@proton.me>
2025-07-04 16:50:35 +0300

    Create plugins.js

---
+++ Scripts/plugins.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/685fddb6b431d41910941002/1ivakjkpn';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
