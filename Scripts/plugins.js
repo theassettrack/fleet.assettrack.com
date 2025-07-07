48dce94b4a6b19c0bb393442b943abe1248a212d
ASTR <theassettrack@proton.me>
2025-07-07 13:10:56 +0200

    Update plugins.js

--- Scripts/plugins.js
+++ Scripts/plugins.js
@@ -1,1 +1,9 @@
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="7f1383ba-70c4-46ea-97bd-9b319a90e60a";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/685fddb6b431d41910941002/1iur7k50t';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
